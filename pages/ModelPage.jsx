99ec616997fc68a146f219a0f9e2ce5be47e2325
Steopa2001 <moraru495@gmail.com>
2025-10-07 18:53:26 +0200

    [feat] add reusable CatalogPage with category filter, sorting, search, and grid/list view

--- pages/ModelPage.jsx
+++ pages/ModelPage.jsx
@@ -1,37 +1,7 @@
-import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import CatalogPage from "./CatalogPage"
 
 const ModelPage = () => {
-  const [products, setProducts] = useState([]);
-  const categoryId = 4; // ID categoria
-
-  useEffect(() => {
-    axios
-      .get(`http://localhost:3000/products/category/${categoryId}`)
-      .then((res) => setProducts(res.data))
-      .catch((err) => console.error(err));
-  }, [categoryId]);
-
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12">
-          <h1>MODELLISMO</h1>
-        </div>
-        {products.map((prod) => (
-          <div key={prod.id} className="col-sm-12 col-md-3 category-card mb-3">
-            <p>{prod.name}</p>
-            <Link to={`/dettaglio-prodotto/${prod.slug}`}>
-              <div className="card-image">
-                <img className="img-fluid" src={prod.image} alt={prod.name} />
-              </div>
-            </Link>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
+  return <CatalogPage fixedCategoryId={4}/>
+}
 
 export default ModelPage;
