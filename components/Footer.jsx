3f063addfd7b9ef8657ffe777cd85ca2ac6a4c50
biancapcr <biancacristina.pocora@gmail.com>
2025-10-09 13:08:57 +0200

    [style] refactor Footer and index.css for improved readability and consistency

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
@@ -8,37 +8,50 @@ const Footer = () => {
         <div className="row">
           <div className="col-footer d-flex pt-5">
             <div className="col-3">
-              <div className='lofo-f'>
+              <div className="lofo-f">
                 <img src="/img/Arcadia-logo.png" alt="logo" className="logo" />
               </div>
-              <div className="text">Arcadia è un e-commerce italiano di giochi da tavolo, carte collezionabili, cardistry e modellismo. Arcadia è un marchio registrato. Ogni utilizzo o riproduzione non espressamente autorizzata è severatamente vietata.</div>
+              <div className="text">
+                Arcadia è un e-commerce italiano di giochi da tavolo, carte
+                collezionabili, cardistry e modellismo. Arcadia è un marchio
+                registrato. Ogni utilizzo o riproduzione non espressamente
+                autorizzata è severatamente vietata.
+              </div>
             </div>
-            <div className="col-3 text-center"><h4>ACCOUNT</h4>
-              <ul className='list-unstyled'>
+            <div className="col-3 text-center">
+              <h4>ACCOUNT</h4>
+              <ul className="list-unstyled">
                 <li>Arcadia Premium</li>
                 <li>Gestioni iscrizioni</li>
               </ul>
             </div>
-            <div className="col-3 text-center"><h4>MENU</h4>
-              <ul className='list-unstyled'>
-                <Link to="/" className='link'><li>Home</li>
+            <div className="col-3 text-center">
+              <h4>MENU</h4>
+              <ul className="list-unstyled">
+                <Link to="/" className="link">
+                  <li>Home</li>
                 </Link>
                 <li>Arcadia premium</li>
-                <Link to="/giochi-da-tavolo" className='link'>
+                <Link to="/giochi-da-tavolo" className="link">
                   <li>Giochi da tavolo</li>
                 </Link>
-                <Link to="/carte-collezionabili" className='link'>
-                  <li>Carte collezionabili</li></Link>
-                <Link to="cardistry" className='link'>
-                  <li>Cardistry</li></Link>
-                <Link to="modellismo" className='link'>
-                  <li>Modellismo</li></Link>
+                <Link to="/carte-collezionabili" className="link">
+                  <li>Carte collezionabili</li>
+                </Link>
+                <Link to="cardistry" className="link">
+                  <li>Cardistry</li>
+                </Link>
+                <Link to="modellismo" className="link">
+                  <li>Modellismo</li>
+                </Link>
                 <li>Promo</li>
               </ul>
             </div>
-            <div className="col-3 text-center"><h4>ARCADIA</h4>
-              <ul className='list-unstyled'>
-                <Link to="/" className='link'><li>Home</li>
+            <div className="col-3 text-center">
+              <h4>ARCADIA</h4>
+              <ul className="list-unstyled">
+                <Link to="/" className="link">
+                  <li>Home</li>
                 </Link>
                 <li>Chi siamo</li>
                 <li>Contattaci</li>
@@ -48,14 +61,15 @@ const Footer = () => {
           </div>
           <div className="col-12 mt-5 mb-5">
             <div className="icons">
-              <i className="fa-brands fa-square-instagram fa-1x"></i>
-              <i className="fa-brands fa-facebook fa-1x"></i>
+              <i class="fa-brands fa-instagram"></i>
+              <i class="fa-brands fa-facebook-f"></i>
+              <i class="fa-brands fa-discord"></i>
             </div>
           </div>
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
