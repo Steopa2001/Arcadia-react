8a0a12a92f9725de08277583017cbaf43ef43cbb
biancapcr <biancacristina.pocora@gmail.com>
2025-10-14 19:05:54 +0200

    [fix] Refactor Thankspage layout and enhance responsive styles

--- pages/Thankspage.jsx
+++ pages/Thankspage.jsx
@@ -1,17 +1,12 @@
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-
 const Thankspage = () => {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12 text-center mt-5 mb-5">
-          <h1>Grazie per aver acquistato da Arcadia!</h1>
-          <p>A breve ti arrivererà un'email con tutti i dettagli dell'ordine</p>
-        </div>
+    <div className="thankspage-container">
+      <div className="container text-center mt-5 mb-5">
+        <h1>Grazie per aver acquistato da Arcadia!</h1>
+        <p>A breve ti arriverà un'email con tutti i dettagli dell'ordine</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Thankspage
+export default Thankspage;
