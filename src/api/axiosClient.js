06685462a13f34596c06da1c338fbbb5f8984e47
Steopa2001 <moraru495@gmail.com>
2025-11-23 14:04:57 +0100

    fix: format axios import and clean up code

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from  "axios";
 
 // Legge l'indirizzo del backend dall'ambiente (.env o Railway)
 const API = import.meta.env.VITE_API_URL;
@@ -8,6 +8,6 @@ const axiosClient = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
-});
+});    
 
 export default axiosClient;
